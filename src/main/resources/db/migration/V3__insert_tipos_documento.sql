d8f7bc854b1409a0f164f7586f54c471fd5cda47
angiehuertas16 <angiehuertas.16@gmail.com>
2026-04-10 23:55:39 -0500

    uso de flyway para poblar bd

---
+++ src/main/resources/db/migration/V3__insert_tipos_documento.sql
@@ -0,0 +1,8 @@
+INSERT INTO tipo_documento (codigo, descripcion)
+VALUES ('CC','Cedula de ciudadania');
+
+INSERT INTO tipo_documento (codigo, descripcion)
+VALUES ('CE','Cedula de extranjeria');
+
+INSERT INTO tipo_documento (codigo, descripcion)
+VALUES ('PAS','Pasaporte');
---
+++ src/main/resources/db/migration/V4__insert_tipos_credito.sql
@@ -0,0 +1,8 @@
+INSERT INTO tipo_credito (rate, descripcion)
+VALUES (1.5, 'Credito personal');
+
+INSERT INTO tipo_credito (rate, descripcion)
+VALUES (2.1, 'Credito vehiculo');
+
+INSERT INTO tipo_credito (rate, descripcion)
+VALUES (1.2, 'Credito vivienda');
